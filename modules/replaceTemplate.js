2be2323afe6e11154517a47ad5e874273513919b
abidxcrescent <101678943+abidxcrescent@users.noreply.github.com>
2022-09-04 12:51:02 -0700

    Add files via upload

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,11 +1,14 @@
 
-module.exports = (htmlStr, course)=>{ // fat arrow function or lambda
-    let output = htmlStr.replace(/{%NAME%}/g, course.courseName);
-    output = output.replace(/{%IMAGE%}/g, course.image);
-    output = output.replace(/{%FROM%}/g, course.from);
-    output = output.replace(/{%INSTRUCTOR%}/g, course.instructor);
-    output = output.replace(/{%CREDITS%}/g, course.credits);
-    output = output.replace(/{%DESCRIPTION%}/g, course.description);
-    output = output.replace(/{%ID%}/g, course.id);
+module.exports = (htmlStr, loan)=>{ // fat arrow function or lambda
+    let output = htmlStr.replace(/{%CUSTOMERNAME%}/g, loan.customerName);
+    output = output.replace(/{%PHONENUMBER%}/g, loan.phoneNumber);
+    output = output.replace(/{%ADDRESS%}/g, loan.address);
+    output = output.replace(/{%LOANAMOUNT%}/g, loan.loanAmount);
+    output = output.replace(/{%INTEREST%}/g, loan.interest);
+    output = output.replace(/{%LOANTERMSYEARS%}/g, loan.loanTermYears);
+    output = output.replace(/{%LOANTYPE%}/g, loan.loanType);
+    output = output.replace(/{%DESCRIPTION%}/g, loan.description);
+    let ans = loan.loanAmount*loan.loanTermYears
+    output = output.replace(/{%ANS%}/g, loan.ans);
     return output;
 }
